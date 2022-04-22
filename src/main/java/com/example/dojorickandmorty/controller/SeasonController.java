d5547c4a7bc1f7f81a1e7f8ab33124e561e760bd
Pawel Gorski <pawel.gorski@envelo.pl>
2022-04-22 09:04:23 +0200

    CrossOrigin

--- src/main/java/com/example/dojorickandmorty/controller/SeasonController.java
+++ src/main/java/com/example/dojorickandmorty/controller/SeasonController.java
@@ -9,15 +9,13 @@ import com.example.dojorickandmorty.service.SeasonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(value = "https://krzt96.github.io/rickmorty/")
 public class SeasonController {
     private final EpisodeService episodeService;
     private final SeasonService seasonService;
